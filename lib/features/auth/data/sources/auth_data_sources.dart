458dd061eb7882e7275c76fb59b7f231b0596cfc
Dpi-001 <sudip9845197918@gmail.com>
2025-05-30 12:13:00 +0545

    ApiConstants.registerUserRoute

---
+++ lib/features/auth/data/sources/auth_data_sources.dart
@@ -0,0 +1,20 @@
+import 'package:smartapi/core/apis/api_calls.dart';
+
+abstract class AuthDataSources {
+  Future<String> registeruser({required Map<String, dynamic> userData});
+}
+
+class AuthDataSourcesImpl extends AuthDataSources {
+  final ApiCalls apiCalls;
+
+  AuthDataSourcesImpl({required this.apiCalls});
+
+  @override
+  Future<String> registeruser({required Map<String, dynamic> userData}) async {
+    final response = await apiCalls.postData(
+      endpoint: 'ApiConstants.registerUserRoute',
+      data: userData,
+    );
+    return 'User registered successfully: ${response['message']}';
+  }
+}
