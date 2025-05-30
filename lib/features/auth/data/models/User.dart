5b57e52695517eef884da1fb32a9c57202d7980a
Dpi-001 <sudip9845197918@gmail.com>
2025-05-30 12:11:59 +0545

    Update: Json serializable

--- lib/features/auth/data/models/User.dart
+++ lib/features/auth/data/models/User.dart
@@ -1,12 +1,14 @@
-class User {
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
 
+class User {
   String? fullName;
   String? email;
   String? password;
   String? address;
   String? phoneNumber;
 
-User({
+  User({
     this.fullName,
     this.email,
     this.password,
@@ -14,6 +16,29 @@ User({
     this.phoneNumber,
   });
 
- 
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'fullName': fullName,
+      'email': email,
+      'password': password,
+      'address': address,
+      'phoneNumber': phoneNumber,
+    };
+  }
 
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      fullName: map['fullName'] != null ? map['fullName'] as String : null,
+      email: map['email'] != null ? map['email'] as String : null,
+      password: map['password'] != null ? map['password'] as String : null,
+      address: map['address'] != null ? map['address'] as String : null,
+      phoneNumber:
+          map['phoneNumber'] != null ? map['phoneNumber'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
 }
