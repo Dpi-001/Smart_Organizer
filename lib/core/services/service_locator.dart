c4f5b7e9629693bd4e0ad6d20c5f2432beb05ea2
Dpi-001 <sudip9845197918@gmail.com>
2025-05-30 12:10:46 +0545

    Update : setupServiceLocator

---
+++ lib/core/services/service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:get_it/get_it.dart';
+import 'package:smartapi/core/apis/api_calls.dart';
+import 'package:smartapi/features/auth/data/repositories/auth_repositories.dart';
+import 'package:smartapi/features/auth/data/sources/auth_data_sources.dart';
+
+class ServiceLocator {
+  static GetIt getIt = GetIt.instance;
+
+  static setupServiceLocator() {
+    getIt.registerLazySingleton<ApiCalls>(() => ApiCalls());
+    getIt.registerLazySingleton<AuthDataSources>(
+      () => AuthDataSourcesImpl(apiCalls: getIt<ApiCalls>()),
+    );
+    getIt.registerLazySingleton<AuthRepositories>(
+      () => AuthRepositoriesImpl(authDataSources: getIt<AuthDataSources>()),
+    );
+  }
+}
