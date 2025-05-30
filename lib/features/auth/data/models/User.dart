c6add9740ce799f6236b62a46f4fb9b54c6b2e63
Dpi-001 <sudip9845197918@gmail.com>
2025-05-30 08:00:56 +0545

    User: to database

---
+++ lib/features/auth/data/models/User.dart
@@ -0,0 +1,19 @@
+class User {
+
+  String? fullName;
+  String? email;
+  String? password;
+  String? address;
+  String? phoneNumber;
+
+User({
+    this.fullName,
+    this.email,
+    this.password,
+    this.address,
+    this.phoneNumber,
+  });
+
+ 
+
+}
