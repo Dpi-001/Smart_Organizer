175448cc2dbce5fd0ab7101f4490ddc9363854ea
Dpi-001 <sudip9845197918@gmail.com>
2025-06-03 22:05:48 +0545

    Update : apibaseurl

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String apiBaseUrl = "";
+  static const String apiBaseUrl = 'http://192.168.254.5:8000/api/v1/';
 
-  static const String registerUserRoute = "/register";
+  static const String registerUserRoute = 'register';
 }
