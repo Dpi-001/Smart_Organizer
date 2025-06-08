be189c91d9a56e15ae63cd1d7533518c11fea2aa
Dpi-001 <sudip9845197918@gmail.com>
2025-06-08 22:13:04 +0545

    Updated :   AppRoutes.homeScreen: (context) => const HomeScreen(),

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         routes: {
           AppRoutes.loginScreen: (context) => LoginScreen(),
           AppRoutes.registerScreen: (context) => RegisterScreen(),
-          AppRoutes.homeScreen: (context) => const HomeSceen(),
+          AppRoutes.homeScreen: (context) => const HomeScreen(),
         },
       ),
     );
